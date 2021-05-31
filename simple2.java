6504d1a1a3cdc019ed5c6bc121341ff1dcf8ea7a
reem <reem.atalah1@gmail.com>
2021-05-31 23:56:49 +0200

    length of page words

--- simple2.java
+++ simple2.java
@@ -6,38 +6,6 @@ import org.jsoup.select.Elements;
 
 public class simple2 {
 
-    private static Boolean ReadFromFile(String word) {//take care! I'm ignored!
-    LineNumberReader reader =null;
-    try {
-        reader = new LineNumberReader(new FileReader(new File("ignoredWords.txt")));
-        String str;
-        while ((str=reader.readLine()) != null) //read file till the end
-        {
-            if(str.equals(word))
-            {
-                if(reader != null){
-                    try {
-                      reader.close();
-                    } catch (IOException e) {
-                      e.printStackTrace();
-                    }
-                }
-                return false;
-            }
-        }
-        
-    } catch (Exception ex) {
-        ex.printStackTrace();
-    }
-    if(reader != null){
-        try {
-          reader.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-    }
-    return true;
-    }
     public static void main(String[] args) throws IOException {
         //String html= "<html><head><title> first parser </title></head> <body>  <h1> Hi </h1><p>paragraph 1. I'm reem I hope you are fine.</p></body></html>";
         
@@ -47,10 +15,13 @@ public class simple2 {
             // System.out.println(doc.body().text());
             // System.out.println(doc.body().tagName());
             // System.out.println(doc.wholeText());
+            String[] GetNumbOfWords=doc.text().split(" ");
+            System.out.println(GetNumbOfWords.length);
+            // System.out.println(doc.text());
 
             Elements elements = doc.select("*");
             for(Element e : elements)
-                if( e.tagName() != "body" && e.tagName() !="#root" && e.tagName() !="html" && e.tagName() !="meta" && e.tagName() !="style")
+                if( e.tagName() != "body" && e.tagName() !="#root" && e.tagName() !="html" && e.tagName() !="style")
                     System.out.println( e.tagName() + ": " + e.text());
 
             // System.out.println(doc.getElementsContainingOwnText("illustrative"));
