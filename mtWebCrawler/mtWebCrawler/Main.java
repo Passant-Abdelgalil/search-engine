f06674f546edd8a31e82d6a97460216c69eaf971
Abeerhbadr <abeer.h.badr@gmail.com>
2021-06-01 23:17:24 +0200

    crawler so far

--- mtWebCrawler/mtWebCrawler/Main.java
+++ mtWebCrawler/mtWebCrawler/Main.java
@@ -7,13 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //System.out.println("please enter number of threads");
-        Scanner s = new Scanner(System.in);
-        //int n = s.nextInt();
-        ArrayList<WebCrawler> bots = new ArrayList<WebCrawler>();
+        mtCrawler mtc = new mtCrawler();
+        mtc.crawl(4);
 
-        //FileFilling.fileCreating();
-        FileFilling.fileWriterCreation();
+        //System.out.println("please enter number of threads");
+        // Scanner s = new Scanner(System.in);
+        // //int n = s.nextInt();
+        // ArrayList<WebCrawler> bots = new ArrayList<WebCrawler>();
+
+        // FileFilling file = new FileFilling();
+        // //FileFilling.fileCreating();
+        // file.fileWriterCreation();
 
         // try {
         //     Thread.currentThread().join();
@@ -29,20 +33,34 @@ public class Main {
         
         //for(thread : threads)
 
-        bots.add(new WebCrawler("https://en.wikipedia.org/wiki/Web_crawler", 1, 1));
-        bots.add(new WebCrawler("https://moz.com/beginners-guide-to-seo/how-search-engines-operate", 2, 2));
-        bots.add(new WebCrawler("https://www.searchenginejournal.com/search-engines/ranking/", 3, 3));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/Web_crawler", 1, 1, file));
+        // bots.add(new WebCrawler("https://moz.com/beginners-guide-to-seo/how-search-engines-operate", 2, 2, file));
+        // bots.add(new WebCrawler("https://www.searchenginejournal.com/search-engines/ranking/", 3, 3, file));
+        // bots.add(new WebCrawler("https://www.techopedia.com/definition/28064/search-engine-query", 4, 4, file));
+        // bots.add(new WebCrawler("https://www.w3schools.com/whatis/", 5, 5, file));
+        // bots.add(new WebCrawler("https://www.guru99.com/java-static-variable-methods.html", 6, 6, file));
+        // bots.add(new WebCrawler("https://softwareengineering.stackexchange.com/questions/185527/how-to-find-a-good-seed-page-for-a-web-crawler", 7, 7, file));
+        // bots.add(new WebCrawler("https://nlp.stanford.edu/IR-book/html/htmledition/crawling-1.html", 8, 8, file));
+        // bots.add(new WebCrawler("https://www.guru99.com/multithreading-java.html", 9, 9, file));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/Operating_system", 10, 10, file));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/NoSQL", 11, 11, file));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/Inverted_index", 12, 12, file));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/MongoDB", 13, 13, file));
+        // bots.add(new WebCrawler("https://www.igi-global.com/dictionary/adaptive-query-processing-data-grids/24374", 14, 14, file));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/Phrase_search", 15, 15, file));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/Jsoup", 16, 16, file));
+        // bots.add(new WebCrawler("https://en.wikipedia.org/wiki/Elasticsearch", 17, 17, file));
 
-        for(WebCrawler w: bots){
-            try {
-                w.getThread().join();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
+        // for(WebCrawler w: bots){
+        //     try {
+        //         w.getThread().join();
+        //     } catch (InterruptedException e) {
+        //         // TODO Auto-generated catch block
+        //         e.printStackTrace();
+        //     }
+        // }
 
-        FileFilling.fileClosing();
+        //FileFilling.fileClosing();
 
     }
 }
--- mtWebCrawler/mtWebCrawler/WebCrawler.java
+++ mtWebCrawler/mtWebCrawler/WebCrawler.java
@@ -1,7 +1,16 @@
 package mtWebCrawler;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URI;
+import java.net.URLConnection;
+//import java.net.HttpURLConnection;
 import java.util.ArrayList;
+
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import java.net.URL;
+// import java.net.URLEncoder;
 
 //import javax.swing.text.Document;
 
@@ -10,101 +19,29 @@ import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.*;
-
-class FileFilling {
-
-    ArrayList<String> visitedLinks = new ArrayList<String>(); // el mfrood a7ot synch 3ala di, wl file, wl level
-                                                              // n7ot impl. el crawl f class? wlla class
-                                                              // FileFilling
-
-    int level;
-    int lineInFile;
-    static File myObj;
-    static FileWriter myWriter;
-
-    static void fileCreating() { 
-        try {
-            myObj = new File("filename.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException ex) {
-            System.out.println("An error occurred.");
-            ex.printStackTrace();
-        }
-    }
-
-    static void fileWriterCreation() {  //quikfix made me make it static
-        try {
-            myWriter = new FileWriter("filename.txt");
-            // myWriter.write(URL);
-
-        } catch (IOException e) {
-            System.out.println("An error occurred when Write to file.");
-        }
-
-    }
-
-    synchronized void WriteToFile(String URL) {
-        try {
-            myWriter.write(URL + "\n");
-
-        } catch (IOException e) {
-            System.out.println("An error occurred when Write to file.");
-        }
-    }
-
-    static void fileClosing() {
-        try {
-            myWriter.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    synchronized String ReadFromFile(int n) {
-        try (Stream<String> lines = Files.lines(Paths.get("filename.txt"))) {
-            String link = lines.skip(n).findFirst().get();
-            return link;
-        } catch (IOException e) {
-            System.out.println("An error when Read from file.");
-            return "";
-        }
-    }
-
-    synchronized void levelIncrement() {
-        level++;
-    }
-
-    synchronized int lineInFileIncrement() {
-        return lineInFile++;
-    }
-
-}
+import mtWebCrawler.BreadthFirstSearchExampleNeighbourList.urlObj;
 
 public class WebCrawler implements Runnable {
 
-    private static final int MAX_DEPTH = 5000;
+    //private static final int MAX_DEPTH = 5000;
     private Thread thread;
-    private String first_link;
+    //private String first_link;
+
     private int ID;
-    private int startLine;
+    private RobotChecker robotChecker;
+    BreadthFirstSearchExampleNeighbourList bfsQueue;
+    //private int startLine;
 
-    FileFilling file = new FileFilling();
+    //FileFilling file;
 
-    public WebCrawler(String link, int num, int startLine) {
+    public WebCrawler(BreadthFirstSearchExampleNeighbourList bfsQueue, int num, RobotChecker checker) {
         System.out.print("WebCrawler created");
-        first_link = link;
+        //first_link = link;
         ID = num;
-        this.startLine = startLine;
+        this.bfsQueue=bfsQueue;
+        robotChecker = checker;
+        //this.startLine = startLine;
+        //this.file = file;
 
         thread = new Thread(this);
         thread.start();
@@ -120,36 +57,91 @@ public class WebCrawler implements Runnable {
             e.printStackTrace();
         }
 
-        file.WriteToFile(first_link);
+        //file.WriteToFile(first_link);
 
-        crawl(startLine);
+        // crawl(startLine);
 
+        crawl();
 
     }
 
-    private void crawl(int lineInFile) { // level init =1 LineinFile init=1
+    private void crawl() { // level init =1 LineinFile init=1
 
-        if (file.level <= MAX_DEPTH) { // untill we have 5000 link
-            System.out.println("want to read");
-            String URL = file.ReadFromFile(lineInFile);
-            if (URL == "")
-                System.out.println("didn't read");
+        if (bfsQueue.count <= 5000) { // untill we have 5000 link //&& file.count<=5000 8alat
 
-            Document doc = request(URL);
+            urlObj top = bfsQueue.getQTop();
+            Document doc = request(top.url);
+            //Document doc = null;
+            // try {
+            //     URLConnection uConn = new URL(bfsQueue.getQTop().url).openConnection();
+            //     String contentType = uConn.getHeaderField("Content-Type");
+            //     if (contentType.equals("text/html; charset=utf-8") || contentType.equals("text/html")) {
+            //         doc = request(top.url);
+            //         System.out.println("HTML");
+            //     }
+            // } catch (Exception e) {
+            //     System.out.println(e.getMessage());
+            // }
+
             if (doc != null) {
                 for (Element link : doc.select("a[href]")) {
                     String next_link = link.absUrl("href");
-
-                    if (file.visitedLinks.contains(next_link) == false) {
-                        file.WriteToFile(next_link);
-                        file.levelIncrement();
+                    //next_link = URI.create(next_link).normalize().toString();
+                    try {
+                        robotChecker.getRules(next_link);
+                        //System.out.println(next_link);
+                    } catch (Exception e) {
+                        System.out.println("Error while checking robots.txt! " + e.getMessage());
+                        continue;
                     }
-                }
-                crawl(file.lineInFileIncrement());
-            }
-        }
 
+                    urlObj URLOBJJ = new urlObj(next_link);
+                    top.addneighbours(URLOBJJ);                    
+
+                }
+
+                bfsQueue.bfs(top);
+            }
+
+            crawl();
+            //bfsQueue.bfs(bfsQueue.getQTop());
+        }
     }
+
+    // private void crawl(int level, String URL) { // level init =1 LineinFile
+    // init=1
+
+    // if (level <= MAX_DEPTH) { // untill we have 5000 link //&& file.count<=5000
+    // 8alat
+    // // System.out.println("want to read");
+    // // String URL = file.ReadFromFile(lineInFile);
+    // // if (URL == "")
+    // // System.out.println("didn't read");
+
+    // Document doc = request(URL);
+    // if (doc != null) {
+    // for (Element link : doc.select("a[href]")) {
+    // String next_link = link.absUrl("href");
+
+    // if (file.visitedLinks.contains(next_link) == false) {
+    // file.countIncrement();
+    // if (file.count <= 5000) {
+
+    // // file.WriteToFile(next_link);
+    // crawl(level++, next_link);
+
+    // } else {
+    // return;
+    // }
+
+    // // file.levelIncrement();
+    // }
+    // }
+    // // crawl(file.lineInFileIncrement());
+    // }
+    // }
+
+    // }
 
     private Document request(String url) {
         try {
@@ -158,12 +150,47 @@ public class WebCrawler implements Runnable {
 
             if (con.response().statusCode() == 200) {
                 System.out.println("\n**Bot ID:" + ID + " Received Webpage at " + url);
+                // file.WriteToFile(url);
+                // if (file.count <= 5000) {
+
+                //     file.WriteToFile(url);
+                // }
 
                 String title = doc.title();
                 System.out.println(title);
-                file.visitedLinks.add(url);
+                // synchronized (this.file) {
+                //     file.visitedLinks.add(url);
+                // }
 
                 return doc;
+
+                // HttpURLConnection urlc = (HttpURLConnection)url.openConnection();
+                // urlc.setAllowUserInteraction( false );
+                // urlc.setDoInput( true );
+                // urlc.setDoOutput( false );
+                // urlc.setUseCaches( true );
+                // urlc.setRequestMethod("HEAD");
+                // urlc.connect();
+                // String mime = urlc.getContentType();
+                // if(mime.equals("text/html") {
+                // // do your stuff
+
+                // System.out.println("\n**Bot ID:" + ID + " Received Webpage at " + url);
+                // // file.WriteToFile(url);
+                // if (file.count <= 5000) {
+
+                // file.WriteToFile(url);
+                // }
+
+                // String title = doc.title();
+                // System.out.println(title);
+                // synchronized (this.file) {
+                // file.visitedLinks.add(url);
+                // }
+
+                // return doc;
+                // }
+
             }
             return null;
         } catch (IOException e) {
